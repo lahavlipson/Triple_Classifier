7f68c80ffc22e2608e86f0d6680e03748c1931c7
Lahav174 <lahav.lipson@columbia.edu>
2017-10-08 18:52:52 -0400

    Altered functions so that they return output based on function parameters, as opposed to using global vars

--- TripleClassifier.py
+++ TripleClassifier.py
@@ -2,31 +2,25 @@ import numpy as np
 import random
 
 
-vectorDict = {}
-listOfRelations = []
-
-#Initialize array of sets of triples. The set at each index contains all the triples 
-#for the graph whose ID is the index
-tripleArr = np.repeat(set(),36519)
-for i in range(0,len(tripleArr)):
-    tripleArr[i] = set()
-
-setOfGoodTriples = set()
-setOfBadTriples = set()
 
 def setupVectors():#Creates dictionary of vectors and their embedding vectors
+    vectorDict = {}
     with open("vectors.txt") as f:
         for line in f:
             line = line.rstrip()
             arr = line.split(' ')
            # print(arr[0])
             vectorDict[arr[0]] = list(map(float,arr[1:]))
+    return vectorDict
   
-def setUpTripleArr():#Creates an array of sets of triples
+def setUpTripleDict():#Creates an array of sets of triples
+    tripleDict = {}
     with open("deft-p2-amr-r2-training-ALL.triples") as f:
         for line in f:      
             trip = line.rstrip().split(' ')
             graphNum = int(trip[0])
+            if graphNum not in tripleDict:
+                tripleDict[graphNum] = set()
             if trip[2][0:2] != 'op' and trip[2][0:4] != 'name':
                 lst = trip[1:]
                 #Removes -03, -01, etc. for cases like Jump-02
@@ -36,35 +30,38 @@ def setUpTripleArr():#Creates an array of sets of triples
                     lst[0] = lst[0][0:indexOfDash1]
                 if indexOfDash2 >= 0:
                     lst[2] = lst[0][2:indexOfDash2]
-                tripleArr[graphNum].add(tuple(lst))
+                tripleDict[graphNum].add(tuple(lst))
+    return tripleDict
 
-def setUpRelations():#Creates a list of all the possible relations (About 100)
+def setUpRelations(tripDict):#Creates a list of all the possible relations (About 100)
     relationSet = set()
-    for st in tripleArr:#For set in tripleArr
-        for trip in st: #For triple in set
+    for key in tripDict:#For set in tripleDict
+        for trip in tripDict[key]: #For triple in set
             relationSet.add(trip[1])
-    global listOfRelations
-    listOfRelations = list(relationSet)
+    return list(relationSet)
 
 
-def getOneHotEncodingOfRelation(rel):#Returns [0,...,0,1,0,...,0] 
+def getOneHotEncodingOfRelation(rel,listOfAllRels):#Returns [0,...,0,1,0,...,0] 
     output = []
-    for i in range(0,len(listOfRelations)):
-        if listOfRelations[i] == rel:
+    for i in range(0,len(listOfAllRels)):
+        if listOfAllRels[i] == rel:
             output.append(1);
         else:
             output.append(0)
     return np.array(output)
 
-def setUpGoodTriples():#Just compiles all of the triple from tripleArr into a set
-    for i in range(0,len(tripleArr)):
-        setOfTriples = tripleArr[i]
+def setUpGoodTriples(tripDict):#Just compiles all of the triple from tripleDict into a set
+    setOfGoodTriples = set()
+    for i in tripDict:
+        setOfTriples = tripDict[i]
         for trip in setOfTriples:
             setOfGoodTriples.add(trip)
+    return setOfGoodTriples
 
 def setUpBadTriples():#Recombines triples to form bad ones
-    for i in range(0,len(tripleArr)):
-        listOfTriples = list(tripleArr[i])
+    setOfBadTriples = set()
+    for i in tripleDict:
+        listOfTriples = list(tripleDict[i])
         if (len(listOfTriples) >= 2):
         #Need two triples in order to create a good and bad one
             for j in range(0,len(listOfTriples)):
@@ -76,30 +73,30 @@ def setUpBadTriples():#Recombines triples to form bad ones
                 assert(len(secondTriple) == len(firstTriple)); assert(len(secondTriple) == 3)
                 newBadTriple = firstTriple[0:2] + secondTriple[2:]
                 setOfBadTriples.add(newBadTriple)
+    return setOfBadTriples
     
     
-def turnTripleIntoGiantVector(triple):#Returns a vector that can be trained on. Returns None if the first or last word is not in the embedding
+def turnTripleIntoGiantVector(triple, allRelations):#Returns a vector that can be trained on. Returns None if the first or last word is not in the embedding
     if triple[0] in vectorDict and triple[2] in vectorDict:
         vec1 = vectorDict[triple[0]]
-        vec2 = getOneHotEncodingOfRelation(triple[1])
+        vec2 = getOneHotEncodingOfRelation(triple[1],allRelations)
         vec3 = vectorDict[triple[2]]
         return np.concatenate((vec1, vec2, vec3))
     else:
         return None
     
-setupVectors()
-setUpTripleArr()
-setUpRelations()
-setUpGoodTriples()
-setUpBadTriples()
+    
+vectorDict = setupVectors()
+tripleDict = setUpTripleDict()
+listOfRelations = setUpRelations(tripleDict)
+setOfGoodTriples = setUpGoodTriples(tripleDict)
+setOfBadTriples = setUpBadTriples()
 print(len(setOfGoodTriples))#Number tuples
 print(len(setOfBadTriples))#NOTE: There are many more bad tuples than good tuples because many of the good tuples are identical
-print(turnTripleIntoGiantVector(('job', 'topic', 'that'))[45:55])#Testing
-
-
+print(turnTripleIntoGiantVector(('job', 'topic', 'that'),listOfRelations)[45:55])#Testing
 count = 0
 for trip in setOfGoodTriples:
-    h = turnTripleIntoGiantVector(trip)
+    h = turnTripleIntoGiantVector(trip, listOfRelations)
     if h is not None:
         count+=1
 print(count)#This is the number of triples that contain a word not in the word embedding
