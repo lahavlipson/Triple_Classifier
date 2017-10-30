42b597c7ef3dbc376232f1fddd3d5ae55012b813
Lahav174 <lahav.lipson@columbia.edu>
2017-10-29 22:53:01 -0400

    Server commands updated. Keras NN now functions on the cloud

--- KerasClassifier.py
+++ KerasClassifier.py
@@ -10,8 +10,17 @@ numOfData = 277500
 dataDim = 145
 
 
-# load pima indians dataset
-dataset = np.loadtxt("/Users/lahavlipson/Research/Triple_Classifier/trainingData.txt", delimiter=",")
+
+
+dataset = np.zeros(numOfData*(dataDim+1))
+dataset.shape = (numOfData, dataDim+1)
+lineNum = 0
+with open("/Users/lahavlipson/Research/Triple_Classifier/trainingData.txt") as f:
+    for line in f:
+        lArr = line.split(',')
+        assert(len(lArr)==146)
+        dataset[lineNum] = lArr    
+        lineNum+=1
 
 #Shuffles rows of data
 np.random.shuffle(dataset)
@@ -26,10 +35,12 @@ Y_test = dataset[int(numOfData*0.8):,dataDim]
 # create model
 model = Sequential()
 model.add(Dense(150, input_dim=dataDim, activation='relu'))
+model.add(Dense(150, activation='relu'))
+model.add(Dense(150, activation='relu'))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(100, activation='relu'))
-#model.add(Dense(100, activation='relu'))
 model.add(Dense(100, activation='relu'))
+model.add(Dense(80, activation='relu'))
 model.add(Dense(40, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -37,7 +48,7 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-model.fit(X_train, Y_train, epochs=1, batch_size=1)
+model.fit(X_train, Y_train, epochs=25, batch_size=10)
 
 # calculate predictions
 predictions = model.predict(X_test)
